aeab5ff1c68a445fa574da9afd9871e8d379e944
sumit <sharmasumit0308@gmail.com>
2020-10-11 17:51:53 +0530

    passport-facebook

--- authenticate.js
+++ authenticate.js
@@ -5,7 +5,7 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
 var config = require('./config');
-
+var FacebookTokenStrategy = require('passport-facebook-token');
 passport.use( new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 
@@ -48,4 +48,34 @@ exports.verifyUser = passport.authenticate('jwt', {session: false});
         err.status = 403;
         return next(err);       
     }
+};
+
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
 }
+));
+
+
